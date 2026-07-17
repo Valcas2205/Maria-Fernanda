c0b9da05c2d5d547e38c4c12115b07997ccdc034
EibarDevHouse <ehernandez@ladevhouse.com>
2026-07-17 11:18:54 -0400

    fixed build

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
-interface InputProps extends React.ComponentProps<'input'> {
+interface InputProps extends Omit<React.ComponentProps<'input'>, 'size'> {
   size?: 'default' | 'lg'
 }
 
